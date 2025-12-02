0c4a737ebba3b20a45e5bdfcf23d13631a294391
Brewer89 <157563542+Brewer89@users.noreply.github.com>
2025-12-01 21:55:58 -0600

    Update main.cpp
    more comments

--- Final/main.cpp
+++ Final/main.cpp
@@ -1,6 +1,7 @@
 /*
-** isaac coddington 11/29/25 1-430pm, 830-12 = 7hours
-** isaac coddington 11/30/25 1-5 8-1230am = 9.5hours
+** isaac coddington 11/29/25 1-430pm, 830-12 = 7hours starting project, ui layout, creating signal and slots, initial function creation
+** isaac coddington 11/30/25 1-5 8-1230am = 9.5hours in depth working on functions, troubleshooting unintended behavior, creating the charsheet window and more troubleshooting / testing
+** isaac coddington 12/1/25 2 hours manual testing, cleaning up my comments, writing the readme
 */
 #include "mainwindow.h"
 
@@ -13,4 +14,5 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
+
 
